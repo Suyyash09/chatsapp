3a03d5ba381877973b7d9ba9ceadb947aac559e9
Suyyash09 <suyashkanchan@gmail.com>
2026-05-03 06:48:59 +0530

    login & logout functionality

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,11 @@
 import express from "express";
 
-import { signup } from "../controllers/auth.controller.js";
+import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", signup);
-
-router.get("/login", (req, res) => {
-  res.send("login Endpoint");
-});
-
-router.get("/logout", (req, res) => {
-  res.send("logout Endpoint");
-});
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
